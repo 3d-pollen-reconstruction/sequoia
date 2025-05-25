d1f4fcf659ac8da3e6ed9654efc386e9b4252e00
Etienne Roulet <etienne.roulet@outlook.com>
2025-05-25 23:53:05 +0200

    added extract mesh and added hunyuan script
    added extract mesh and added hunyuan script

--- Hunyuan3D-2/examples/fast_shape_gen_pollen_orthogonal.py
+++ Hunyuan3D-2/examples/fast_shape_gen_pollen_orthogonal.py
@@ -6,12 +6,14 @@ from hy3dgen.rembg import BackgroundRemover
 from hy3dgen.shapegen import Hunyuan3DDiTFlowMatchingPipeline
 
 # Directory containing all sample folders
+# edit for your input directory of the orthgonal pollen images
 base_dir = r"C:\Users\super\Documents\Github\sequoia\Pixel_Nerf\pollen\pollen_test"
 
 # Mapping from filename to view key
 filename_to_view = {
     "000000.png": "front",
     "000001.png": "left",
+    "000002.png": "right",
     # Add more mappings if needed
 }
 
@@ -52,4 +54,8 @@ for sample_name in os.listdir(base_dir):
         output_type='trimesh'
     )[0]
     print(f"{sample_name} --- {time.time() - start_time:.2f} seconds ---")
-    mesh.export(os.path.join(base_dir, sample_name, "demo_mv3.glb"))
+    # edit your output directory here
+    output_dir = r"C:\Users\super\Documents\Github\sequoia\data\results\Hunyuan3D"
+    os.makedirs(output_dir, exist_ok=True)
+    #mesh.export(os.path.join(output_dir, f"{sample_name}.glb"))
+    mesh.export(os.path.join(output_dir, f"{sample_name}.stl"))
